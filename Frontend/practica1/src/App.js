7f1724ef759cefe92e2f052def818c84bc63c8fb
Edi Tomas <tomas3320141@gmail.com>
2023-06-05 23:32:40 -0600

    .

--- Frontend/practica1/src/App.js
+++ Frontend/practica1/src/App.js
@@ -1,11 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Table,Container } from 'reactstrap';
+import { Table,Container,FormGroup,Button } from 'reactstrap';
 import { useState, useEffect } from "react";
 import axios from "axios";
 function App() {
     const [consult, setconsult] = useState([]);
+    const [consult2, setconsult2] = useState([]);
+
+    const [data, setdata]=useState([{  num1:0,num2:0}])
+    const [formt, setformt] = useState({num1:0,num2:0});
+  
     const consulta = async () => {
       try{
           const res= await axios.get("http://localhost:4000/list");
@@ -25,6 +30,18 @@ function App() {
   };
 
 
+  const consulta2 = async () => {
+    try{
+        const res= await axios.post("http://localhost:4000/Suma",data);
+        
+        console.log(res.data.result)
+        let rs =res.data;
+        //alert(rs)
+        setconsult2(rs);
+    }catch(ex){
+        console.log(ex);
+    }
+};
 
 
 
@@ -34,8 +51,25 @@ function App() {
 
 
 
+const Insertar=(evt)=>{
+  //console.log(formt)
+  setdata([...data,formt])
+  //consulta2(data);
+     
+}
 
+function handlechange(evt) {
+    const { target } = evt;
+    const { name, value } = target;
 
+    const newValues = {
+      ...formt,
+      [name]: value,
+    };
+
+  // Sincroniza el estado de nuevo
+  setformt(newValues);
+}
 
 
 
@@ -44,6 +78,7 @@ function App() {
 
   useEffect(() => {
       consulta();
+      consulta2();
      
       
 
@@ -54,22 +89,29 @@ function App() {
 
 
 
-  const [data, setdata]=useState([
-    {
-        name:"Edi",
-        url:"Pan",
-    },
-    {
-        name:"Rudy",
-        url:"Une",
-    },
-    ])
+  
+
 
   
   return (
     <>
       <div>
-      
+      <FormGroup>
+            <label>Num1</label>
+            <input className="form-control"  type="number" name="num1" onChange={handlechange}/>
+      </FormGroup>
+      <FormGroup>
+            <label>Num2</label>
+            <input className="form-control"  type="number" name="num2" onChange={handlechange} />
+      </FormGroup>
+      <FormGroup>
+            <label>resultado</label>
+            <input className="form-control"  type="number" name="total" value={consult2} />
+      </FormGroup>
+      <FormGroup>
+            <Button color="success" onClick={Insertar}  >Insertar</Button>{" "}
+      </FormGroup>
+                              
       </div>
       <div>
         <Tabla data={consult}/> 
